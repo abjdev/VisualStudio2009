222a3f472d4eaa42e1aa2c8954b53c7940083dbb
abjdev <91409956+abjdev@users.noreply.github.com>
2021-11-30 19:03:58 +0300

    Release
    Release lol

---
+++ vs2009/vs2009/Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace vs2009
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.kryptonManager1 = new ComponentFactory.Krypton.Toolkit.KryptonManager(this.components);
+            this.kryptonRibbon1 = new ComponentFactory.Krypton.Ribbon.KryptonRibbon();
+            this.kryptonRibbonTab1 = new ComponentFactory.Krypton.Ribbon.KryptonRibbonTab();
+            this.kryptonRibbonGroup1 = new ComponentFactory.Krypton.Ribbon.KryptonRibbonGroup();
+            this.kryptonRibbonGroupTriple1 = new ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupTriple();
+            this.button1 = new ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupButton();
+            this.button2 = new ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupButton();
+            this.button3 = new ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupButton();
+            this.kryptonRibbonTab2 = new ComponentFactory.Krypton.Ribbon.KryptonRibbonTab();
+            this.kryptonRibbonGroup2 = new ComponentFactory.Krypton.Ribbon.KryptonRibbonGroup();
+            this.kryptonRibbonGroupTriple2 = new ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupTriple();
+            this.kryptonRibbonGroupButton1 = new ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupButton();
+            this.kryptonRibbonGroupButton2 = new ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupButton();
+            this.kryptonRibbonGroupSeparator1 = new ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupSeparator();
+            this.kryptonRibbonGroupTriple3 = new ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupTriple();
+            this.kryptonRibbonGroupButton3 = new ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupButton();
+            this.kryptonRibbonGroupButton4 = new ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupButton();
+            this.kryptonRibbonGroupButton5 = new ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupButton();
+            this.kryptonRibbonTab3 = new ComponentFactory.Krypton.Ribbon.KryptonRibbonTab();
+            this.kryptonRibbonGroup3 = new ComponentFactory.Krypton.Ribbon.KryptonRibbonGroup();
+            this.kryptonRibbonGroupTriple4 = new ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupTriple();
+            this.kryptonRibbonGroupButton6 = new ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupButton();
+            this.fastColoredTextBox1 = new FastColoredTextBoxNS.FastColoredTextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.kryptonLabel1 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.autocompleteMenu1 = new AutocompleteMenuNS.AutocompleteMenu();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonRibbon1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // kryptonManager1
+            // 
+            this.kryptonManager1.GlobalPaletteMode = ComponentFactory.Krypton.Toolkit.PaletteModeManager.Office2007Blue;
+            // 
+            // kryptonRibbon1
+            // 
+            this.kryptonRibbon1.InDesignHelperMode = true;
+            this.kryptonRibbon1.Name = "kryptonRibbon1";
+            this.kryptonRibbon1.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.Office2007Blue;
+            this.kryptonRibbon1.RibbonTabs.AddRange(new ComponentFactory.Krypton.Ribbon.KryptonRibbonTab[] {
+            this.kryptonRibbonTab1,
+            this.kryptonRibbonTab2,
+            this.kryptonRibbonTab3});
+            this.kryptonRibbon1.SelectedContext = null;
+            this.kryptonRibbon1.SelectedTab = this.kryptonRibbonTab1;
+            this.kryptonRibbon1.Size = new System.Drawing.Size(948, 135);
+            this.kryptonRibbon1.TabIndex = 0;
+            this.kryptonRibbon1.AppButtonMenuOpened += new System.EventHandler(this.kryptonRibbon1_AppButtonMenuOpened);
+            // 
+            // kryptonRibbonTab1
+            // 
+            this.kryptonRibbonTab1.Groups.AddRange(new ComponentFactory.Krypton.Ribbon.KryptonRibbonGroup[] {
+            this.kryptonRibbonGroup1});
+            this.kryptonRibbonTab1.Text = "File";
+            // 
+            // kryptonRibbonGroup1
+            // 
+            this.kryptonRibbonGroup1.Items.AddRange(new ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupContainer[] {
+            this.kryptonRibbonGroupTriple1});
+            this.kryptonRibbonGroup1.DialogBoxLauncherClick += new System.EventHandler(this.kryptonRibbonGroup1_DialogBoxLauncherClick);
+            // 
+            // kryptonRibbonGroupTriple1
+            // 
+            this.kryptonRibbonGroupTriple1.Items.AddRange(new ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupItem[] {
+            this.button1,
+            this.button2,
+            this.button3});
+            // 
+            // button1
+            // 
+            this.button1.TextLine1 = "Compile";
+            this.button1.Click += new System.EventHandler(this.kryptonRibbonGroupButton1_Click);
+            // 
+            // button2
+            // 
+            this.button2.ImageLarge = global::vs2009.Properties.Resources.Save_icon;
+            this.button2.TextLine1 = "Save";
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.ImageLarge = global::vs2009.Properties.Resources.folder_open_document_icon;
+            this.button3.TextLine1 = "Open";
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // kryptonRibbonTab2
+            // 
+            this.kryptonRibbonTab2.Groups.AddRange(new ComponentFactory.Krypton.Ribbon.KryptonRibbonGroup[] {
+            this.kryptonRibbonGroup2});
+            this.kryptonRibbonTab2.Text = "Edit";
+            // 
+            // kryptonRibbonGroup2
+            // 
+            this.kryptonRibbonGroup2.Items.AddRange(new ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupContainer[] {
+            this.kryptonRibbonGroupTriple2,
+            this.kryptonRibbonGroupSeparator1,
+            this.kryptonRibbonGroupTriple3});
+            this.kryptonRibbonGroup2.TextLine1 = "Editing";
+            // 
+            // kryptonRibbonGroupTriple2
+            // 
+            this.kryptonRibbonGroupTriple2.Items.AddRange(new ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupItem[] {
+            this.kryptonRibbonGroupButton1,
+            this.kryptonRibbonGroupButton2});
+            // 
+            // kryptonRibbonGroupButton1
+            // 
+            this.kryptonRibbonGroupButton1.ImageLarge = global::vs2009.Properties.Resources.Arrow_undo_icon;
+            this.kryptonRibbonGroupButton1.TextLine1 = "Undo";
+            this.kryptonRibbonGroupButton1.Click += new System.EventHandler(this.kryptonRibbonGroupButton1_Click_1);
+            // 
+            // kryptonRibbonGroupButton2
+            // 
+            this.kryptonRibbonGroupButton2.ImageLarge = global::vs2009.Properties.Resources.Arrow_redo_icon;
+            this.kryptonRibbonGroupButton2.TextLine1 = "Redo";
+            this.kryptonRibbonGroupButton2.Click += new System.EventHandler(this.kryptonRibbonGroupButton2_Click);
+            // 
+            // kryptonRibbonGroupTriple3
+            // 
+            this.kryptonRibbonGroupTriple3.Items.AddRange(new ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupItem[] {
+            this.kryptonRibbonGroupButton3,
+            this.kryptonRibbonGroupButton4,
+            this.kryptonRibbonGroupButton5});
+            // 
+            // kryptonRibbonGroupButton3
+            // 
+            this.kryptonRibbonGroupButton3.ImageLarge = global::vs2009.Properties.Resources.Document_Copy_icon;
+            this.kryptonRibbonGroupButton3.TextLine1 = "Copy";
+            this.kryptonRibbonGroupButton3.Click += new System.EventHandler(this.kryptonRibbonGroupButton3_Click);
+            // 
+            // kryptonRibbonGroupButton4
+            // 
+            this.kryptonRibbonGroupButton4.ImageLarge = global::vs2009.Properties.Resources.Paste_icon;
+            this.kryptonRibbonGroupButton4.TextLine1 = "Paste";
+            this.kryptonRibbonGroupButton4.Click += new System.EventHandler(this.kryptonRibbonGroupButton4_Click);
+            // 
+            // kryptonRibbonGroupButton5
+            // 
+            this.kryptonRibbonGroupButton5.ImageLarge = global::vs2009.Properties.Resources.cut_icon;
+            this.kryptonRibbonGroupButton5.TextLine1 = "Cut";
+            this.kryptonRibbonGroupButton5.Click += new System.EventHandler(this.kryptonRibbonGroupButton5_Click);
+            // 
+            // kryptonRibbonTab3
+            // 
+            this.kryptonRibbonTab3.Groups.AddRange(new ComponentFactory.Krypton.Ribbon.KryptonRibbonGroup[] {
+            this.kryptonRibbonGroup3});
+            this.kryptonRibbonTab3.Text = "About";
+            // 
+            // kryptonRibbonGroup3
+            // 
+            this.kryptonRibbonGroup3.Items.AddRange(new ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupContainer[] {
+            this.kryptonRibbonGroupTriple4});
+            // 
+            // kryptonRibbonGroupTriple4
+            // 
+            this.kryptonRibbonGroupTriple4.Items.AddRange(new ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupItem[] {
+            this.kryptonRibbonGroupButton6});
+            // 
+            // kryptonRibbonGroupButton6
+            // 
+            this.kryptonRibbonGroupButton6.ImageLarge = global::vs2009.Properties.Resources.Actions_help_about_icon;
+            this.kryptonRibbonGroupButton6.TextLine1 = "About";
+            this.kryptonRibbonGroupButton6.Click += new System.EventHandler(this.kryptonRibbonGroupButton6_Click);
+            // 
+            // fastColoredTextBox1
+            // 
+            this.fastColoredTextBox1.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+            this.autocompleteMenu1.SetAutocompleteMenu(this.fastColoredTextBox1, this.autocompleteMenu1);
+            this.fastColoredTextBox1.AutoIndentCharsPatterns = "\r\n^\\s*[\\w\\.]+(\\s\\w+)?\\s*(?<range>=)\\s*(?<range>[^;=]+);\r\n^\\s*(case|default)\\s*[^:" +
+    "]*(?<range>:)\\s*(?<range>[^;]+);\r\n";
+            this.fastColoredTextBox1.AutoScrollMinSize = new System.Drawing.Size(31, 18);
+            this.fastColoredTextBox1.BackBrush = null;
+            this.fastColoredTextBox1.BracketsHighlightStrategy = FastColoredTextBoxNS.BracketsHighlightStrategy.Strategy2;
+            this.fastColoredTextBox1.CharHeight = 18;
+            this.fastColoredTextBox1.CharWidth = 10;
+            this.fastColoredTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.fastColoredTextBox1.DefaultMarkerSize = 8;
+            this.fastColoredTextBox1.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.fastColoredTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.fastColoredTextBox1.IndentBackColor = System.Drawing.Color.LightSkyBlue;
+            this.fastColoredTextBox1.IsReplaceMode = false;
+            this.fastColoredTextBox1.Language = FastColoredTextBoxNS.Language.CSharp;
+            this.fastColoredTextBox1.LeftBracket = '(';
+            this.fastColoredTextBox1.LeftBracket2 = '{';
+            this.fastColoredTextBox1.LineNumberColor = System.Drawing.Color.DarkSlateGray;
+            this.fastColoredTextBox1.Location = new System.Drawing.Point(0, 135);
+            this.fastColoredTextBox1.Name = "fastColoredTextBox1";
+            this.fastColoredTextBox1.Paddings = new System.Windows.Forms.Padding(0);
+            this.fastColoredTextBox1.RightBracket = ')';
+            this.fastColoredTextBox1.RightBracket2 = '}';
+            this.fastColoredTextBox1.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.fastColoredTextBox1.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("fastColoredTextBox1.ServiceColors")));
+            this.fastColoredTextBox1.ServiceLinesColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.fastColoredTextBox1.Size = new System.Drawing.Size(948, 424);
+            this.fastColoredTextBox1.TabIndex = 1;
+            this.fastColoredTextBox1.Zoom = 100;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // kryptonLabel1
+            // 
+            this.kryptonLabel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.kryptonLabel1.LabelStyle = ComponentFactory.Krypton.Toolkit.LabelStyle.BoldControl;
+            this.kryptonLabel1.Location = new System.Drawing.Point(0, 535);
+            this.kryptonLabel1.Name = "kryptonLabel1";
+            this.kryptonLabel1.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.Office2007Blue;
+            this.kryptonLabel1.Size = new System.Drawing.Size(948, 24);
+            this.kryptonLabel1.TabIndex = 2;
+            this.kryptonLabel1.Values.ExtraText = "FileType";
+            this.kryptonLabel1.Values.Text = "File";
+            this.kryptonLabel1.Paint += new System.Windows.Forms.PaintEventHandler(this.kryptonLabel1_Paint);
+            // 
+            // autocompleteMenu1
+            // 
+            this.autocompleteMenu1.Colors = ((AutocompleteMenuNS.Colors)(resources.GetObject("autocompleteMenu1.Colors")));
+            this.autocompleteMenu1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.autocompleteMenu1.ImageList = null;
+            this.autocompleteMenu1.Items = new string[] {
+        "public",
+        "private",
+        "internal",
+        "class",
+        "protected",
+        "unsafe",
+        "partial",
+        "override",
+        "readonly",
+        "virtual",
+        "partial",
+        "void",
+        "abstract",
+        "new",
+        "const",
+        "if",
+        "else",
+        "switch",
+        "case",
+        "do",
+        "for",
+        "foreach",
+        "in",
+        "while",
+        "break",
+        "return",
+        "throw",
+        "try",
+        "catch",
+        "finally",
+        "checked",
+        "unchecked",
+        "fixed",
+        "lock",
+        "using"};
+            this.autocompleteMenu1.TargetControlWrapper = null;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(948, 559);
+            this.Controls.Add(this.kryptonLabel1);
+            this.Controls.Add(this.fastColoredTextBox1);
+            this.Controls.Add(this.kryptonRibbon1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Visual Studio 2009";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonRibbon1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ComponentFactory.Krypton.Toolkit.KryptonManager kryptonManager1;
+        private ComponentFactory.Krypton.Ribbon.KryptonRibbon kryptonRibbon1;
+        private ComponentFactory.Krypton.Ribbon.KryptonRibbonTab kryptonRibbonTab1;
+        private ComponentFactory.Krypton.Ribbon.KryptonRibbonGroup kryptonRibbonGroup1;
+        private ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupTriple kryptonRibbonGroupTriple1;
+        private ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupButton button1;
+        private FastColoredTextBoxNS.FastColoredTextBox fastColoredTextBox1;
+        private ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupButton button2;
+        private ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupButton button3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel kryptonLabel1;
+        private AutocompleteMenuNS.AutocompleteMenu autocompleteMenu1;
+        private ComponentFactory.Krypton.Ribbon.KryptonRibbonTab kryptonRibbonTab2;
+        private ComponentFactory.Krypton.Ribbon.KryptonRibbonGroup kryptonRibbonGroup2;
+        private ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupTriple kryptonRibbonGroupTriple2;
+        private ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupButton kryptonRibbonGroupButton1;
+        private ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupButton kryptonRibbonGroupButton2;
+        private ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupSeparator kryptonRibbonGroupSeparator1;
+        private ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupTriple kryptonRibbonGroupTriple3;
+        private ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupButton kryptonRibbonGroupButton3;
+        private ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupButton kryptonRibbonGroupButton4;
+        private ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupButton kryptonRibbonGroupButton5;
+        private ComponentFactory.Krypton.Ribbon.KryptonRibbonTab kryptonRibbonTab3;
+        private ComponentFactory.Krypton.Ribbon.KryptonRibbonGroup kryptonRibbonGroup3;
+        private ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupTriple kryptonRibbonGroupTriple4;
+        private ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupButton kryptonRibbonGroupButton6;
+    }
+}
+
---
+++ vs2009/vs2009/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ComponentFactory.Krypton.Toolkit;
+
+
+// imports for compiling
+using System.CodeDom.Compiler;
+using System.IO;
+using System.Diagnostics;
+using ComponentFactory.Krypton.Ribbon;
+
+namespace vs2009
+{
+    public partial class Form1 : KryptonForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string filepath;
+
+
+        private void compile(string file)
+        {
+            
+            try
+            {
+                if (filepath == "")
+                {
+                    MessageBox.Show("You Need To Save the file to compile");
+                }
+                string exename = "compiled_code.exe";
+                CodeDomProvider provider = CodeDomProvider.CreateProvider("CSharp");
+                CompilerParameters compars = new CompilerParameters();
+
+                compars.GenerateExecutable = true;
+                compars.OutputAssembly = exename;
+                compars.GenerateInMemory = false;
+                compars.TreatWarningsAsErrors = false;
+
+                CompilerResults res = provider.CompileAssemblyFromFile(compars, file);
+                if (res.Errors.Count > 0)
+                {
+                    foreach (CompilerError ce in res.Errors)
+                    {
+                        MessageBox.Show(ce.ToString(), "Error");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Code Compiled.", "Success");
+                    
+                }
+            }
+            catch (System.ArgumentNullException e)
+            {
+                MessageBox.Show("Need to save the file. Expection:" + e.Message, "Visual C# Express 2009",MessageBoxButtons.OK,  MessageBoxIcon.Error);
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "All Files|*.*";
+            save.OverwritePrompt = true;
+            save.CreatePrompt = true;
+
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter Kayit = new StreamWriter(save.FileName);
+                Kayit.WriteLine(fastColoredTextBox1.Text);
+                Kayit.Close();
+                filepath = save.FileName;
+                this.Text = this.Text + " - " + filepath;
+                button1.Enabled = true;
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Title = "Open File";
+            open.Filter = "All Files|*.*";
+
+            if (open.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                var loadedText = File.ReadAllText(open.FileName);
+                fastColoredTextBox1.Text = loadedText;
+                filepath = open.FileName;
+                this.Text = this.Text + " - " + filepath;
+                button1.Enabled = true;
+            }
+        }
+        private void kryptonRibbonGroupButton1_Click(object sender, EventArgs e)
+        {
+            compile(filepath);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fastColoredTextBox1.Text = @"using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DemoApplication
+{
+ class Program
+ { 
+  static void Main(string[] args) 
+  {
+   string text = 
+   Console.Write('Hello World');
+
+   Console.ReadKey();
+   }
+}
+";
+            CheckFormat("needtosave.cs");
+            
+        }
+
+        private void kryptonRibbonGroup1_DialogBoxLauncherClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "All Files|*.*";
+            save.OverwritePrompt = true;
+            save.CreatePrompt = true;
+
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter Kayit = new StreamWriter(save.FileName);
+                Kayit.WriteLine(fastColoredTextBox1.Text);
+                Kayit.Close();
+                filepath = save.FileName;
+                this.Text = this.Text + " - " + filepath;
+                button1.Enabled = true;
+                CheckFormat(save.FileName);
+            }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Title = "Open File";
+            open.Filter = "All Files|*.*";
+
+            if (open.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                var loadedText = File.ReadAllText(open.FileName);
+                fastColoredTextBox1.Text = loadedText;
+                filepath = open.FileName;
+                this.Text = this.Text + " - " + filepath;
+                button1.Enabled = true;
+                kryptonLabel1.Text = open.FileName;
+                CheckFormat(open.FileName);
+
+                
+            }
+        }
+
+        private void kryptonLabel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void CheckFormat(string file)
+        {
+            if (file.EndsWith(".cs"))
+            {
+                kryptonLabel1.Values.ExtraText = "- CSharp";
+            }
+            if (file.EndsWith("Designer.cs"))
+            {
+                kryptonLabel1.Values.ExtraText = "- Winforms Designer(C#)";
+            }
+            if (file.EndsWith(".config"))
+            {
+                kryptonLabel1.Values.ExtraText = "- Configuration";
+            }
+            else
+            {
+                kryptonLabel1.Values.ExtraText = "- Other";
+            }
+        }
+
+        private void kryptonRibbon1_AppButtonMenuOpened(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void kryptonRibbonGroupButton1_Click_1(object sender, EventArgs e)
+        {
+            fastColoredTextBox1.Undo();
+        }
+
+        private void kryptonRibbonGroupButton2_Click(object sender, EventArgs e)
+        {
+            fastColoredTextBox1.Redo();
+        }
+
+        private void kryptonRibbonGroupButton3_Click(object sender, EventArgs e)
+        {
+            fastColoredTextBox1.Copy();
+        }
+
+        private void kryptonRibbonGroupButton4_Click(object sender, EventArgs e)
+        {
+            fastColoredTextBox1.Paste();
+        }
+
+        private void kryptonRibbonGroupButton5_Click(object sender, EventArgs e)
+        {
+            fastColoredTextBox1.Cut();
+        }
+
+        private void kryptonRibbonGroupButton6_Click(object sender, EventArgs e)
+        {
+            AboutBox1 ab = new AboutBox1();
+            ab.ShowDialog();
+        }
+    }
+}
